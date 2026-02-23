2ca7df0427fac244e6ddc8abe4d647a31ffa8c1d
ashwithasaravananonly-rgb <ashwithasaravanan.only@gmail.com>
2026-02-23 17:45:54 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -22,6 +22,10 @@ data = pd.DataFrame({
     "Cholesterol": [chol],
     "Na_to_K": [na_to_k]
 })
+# Encoding (IMPORTANT)
+data['Sex'] = data['Sex'].map({'F':0, 'M':1})
+data['BP'] = data['BP'].map({'LOW':0,'NORMAL':1,'HIGH':2})
+data['Cholesterol'] = data['Cholesterol'].map({'NORMAL':0,'HIGH':1})
 
 if st.button("Predict"):
     prediction = model.predict(data)
