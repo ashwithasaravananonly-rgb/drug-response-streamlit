bf2e0b9b3cb0a8049c27dd6fcc1d39c4dd709324
ashwithasaravananonly-rgb <ashwithasaravanan.only@gmail.com>
2026-02-23 18:53:05 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -9,28 +9,28 @@ st.title("Drug Response Prediction")
 
 # Input fields
 age = st.number_input("Age")
+na = st.number_input("Na")
+k = st.number_input("K")
+
 sex = st.selectbox("Sex", ["M", "F"])
 bp = st.selectbox("BP", ["LOW", "NORMAL", "HIGH"])
 chol = st.selectbox("Cholesterol", ["NORMAL", "HIGH"])
-na_to_k = st.number_input("Na_to_K")
-
-# Convert to dataframe
-data = pd.DataFrame({
-    "Age": [age],
-    "Na_to_K": [na_to_k],
-    "Sex_F": [1 if sex == "F" else 0],
-    "Sex_M": [1 if sex == "M" else 0],
-    "BP_HIGH": [1 if bp == "HIGH" else 0],
-    "BP_LOW": [1 if bp == "LOW" else 0],
-    "BP_NORMAL": [1 if bp == "NORMAL" else 0],
-    "Cholesterol_HIGH": [1 if chol == "HIGH" else 0],
-    "Cholesterol_NORMAL": [1 if chol == "NORMAL" else 0]
-})
-# Encoding (IMPORTANT)
-data['Sex'] = data['Sex'].map({'F':0, 'M':1})
-data['BP'] = data['BP'].map({'LOW':0,'NORMAL':1,'HIGH':2})
-data['Cholesterol'] = data['Cholesterol'].map({'NORMAL':0,'HIGH':1})
 
 if st.button("Predict"):
+
+    data = pd.DataFrame({
+        'Age': [age],
+        'Na': [na],
+        'K': [k],
+        'Sex_F': [1 if sex == "F" else 0],
+        'Sex_M': [1 if sex == "M" else 0],
+        'BP_HIGH': [1 if bp == "HIGH" else 0],
+        'BP_LOW': [1 if bp == "LOW" else 0],
+        'BP_NORMAL': [1 if bp == "NORMAL" else 0],
+        'Cholesterol_HIGH': [1 if chol == "HIGH" else 0],
+        'Cholesterol_NORMAL': [1 if chol == "NORMAL" else 0]
+    })
+
     prediction = model.predict(data)
-    st.success(f"Predicted Drug Type: {prediction[0]}")
+
+    st.success(f"Predicted Drug: {prediction[0]}")
