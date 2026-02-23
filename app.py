176743d7ff3b7e05817461341095f6152970ffc6
ashwithasaravananonly-rgb <ashwithasaravanan.only@gmail.com>
2026-02-23 18:12:49 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -16,16 +16,27 @@ na_to_k = st.number_input("Na_to_K")
 
 # Convert to dataframe
 data = pd.DataFrame({
-    'Age':[age],
-    'Na_to_K':[na_to_k],
-    'Sex_M':[1 if sex=="M" else 0],
-    'Sex_F':[1 if sex=="F" else 0],
-    'BP_LOW':[1 if bp=="LOW" else 0],
-    'BP_NORMAL':[1 if bp=="NORMAL" else 0],
-    'BP_HIGH':[1 if bp=="HIGH" else 0],
-    'Cholesterol_NORMAL':[1 if chol=="NORMAL" else 0],
-    'Cholesterol_HIGH':[1 if chol=="HIGH" else 0],
-})
+data = pd.DataFrame([[ 
+    age,
+    na_to_k,
+    1 if sex=="F" else 0,
+    1 if sex=="M" else 0,
+    1 if bp=="HIGH" else 0,
+    1 if bp=="LOW" else 0,
+    1 if bp=="NORMAL" else 0,
+    1 if chol=="HIGH" else 0,
+    1 if chol=="NORMAL" else 0
+]], columns=[
+    'Age',
+    'Na_to_K',
+    'Sex_F',
+    'Sex_M',
+    'BP_HIGH',
+    'BP_LOW',
+    'BP_NORMAL',
+    'Cholesterol_HIGH',
+    'Cholesterol_NORMAL'
+])
 # Encoding (IMPORTANT)
 data['Sex'] = data['Sex'].map({'F':0, 'M':1})
 data['BP'] = data['BP'].map({'LOW':0,'NORMAL':1,'HIGH':2})
