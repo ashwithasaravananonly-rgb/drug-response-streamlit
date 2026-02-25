570632c546a664c02c30551dfbd6ab6ab9a42ce3
ashwithasaravananonly-rgb <ashwithasaravanan.only@gmail.com>
2026-02-25 18:57:47 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -1,35 +1,36 @@
-import streamlit as st import pickle import pandas as pd # Load model model = pickle.load(open("model.pkl", "rb")) st.title("Drug Response Prediction") # Input fields age = st.number_input("Age") na = st.number_input("Na") k = st.number_input("K") sex = st.selectbox("Sex", ["M", "F"]) bp = st.selectbox("BP", ["LOW", "NORMAL", "HIGH"]) chol = st.selectbox("Cholesterol", ["NORMAL", "HIGH"]) if st.button("Predict"): data = pd.DataFrame({ 'Age': [age], 'Na': [na], 'K': [k], 'Sex_F': [1 if sex == "F" else 0], 'Sex_M': [1 if sex == "M" else 0], 'BP_HIGH': [1 if bp == "HIGH" else 0], 'BP_LOW': [1 if bp == "LOW" else 0], 'BP_NORMAL': [1 if bp == "NORMAL" else 0], 'Cholesterol_HIGH': [1 if chol == "HIGH" else 0], 'Cholesterol_NORMAL': [1 if chol == "NORMAL" else 0] }) prediction = model.predict(data) st.success(f"Predicted Drug: {prediction[0]}")
+import streamlit as st
+import pickle
+import pandas as pd
 
+# Load model
+model = pickle.load(open("model.pkl", "rb"))
 
+st.title("Drug Response Prediction")
 
+# Input fields
+age = st.number_input("Age")
+na = st.number_input("Na")
+k = st.number_input("K")
 
+sex = st.selectbox("Sex", ["M", "F"])
+bp = st.selectbox("BP", ["LOW", "NORMAL", "HIGH"])
+chol = st.selectbox("Cholesterol", ["NORMAL", "HIGH"])
 
+if st.button("Predict"):
 
+    data = pd.DataFrame({
+        'Age': [age],
+        'Na': [na],
+        'K': [k],
+        'Sex_F': [1 if sex == "F" else 0],
+        'Sex_M': [1 if sex == "M" else 0],
+        'BP_HIGH': [1 if bp == "HIGH" else 0],
+        'BP_LOW': [1 if bp == "LOW" else 0],
+        'BP_NORMAL': [1 if bp == "NORMAL" else 0],
+        'Cholesterol_HIGH': [1 if chol == "HIGH" else 0],
+        'Cholesterol_NORMAL': [1 if chol == "NORMAL" else 0]
+    })
 
+    prediction = model.predict(data)
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    st.success(f"Predicted Drug: {prediction[0]}")
